c5609cc34a61c17be81542a2cf0ea33544764045
Saish Fatangare <fatangaresaish30@gmail.com>
2024-03-21 20:24:20 +0530

    Created ThunderClient Collections to Manage Requests

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const mongoURI = "mongodb://0.0.0.0:27017/inotebook?tls=false";
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI)
-}
+const connectToMongo = () => {
+  mongoose.connect(mongoURI);
+};
 
-module.exports = connectToMongo
+module.exports = connectToMongo;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,29 +1,43 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
+//create user using: Post {/api/auth/createuser}
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "enter a valid email").isEmail(),
+    body("password", "enter a valid password").isLength({ min: 3 }),
+  ],
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    //check whether the user exixts already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "sorry a user with this email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
 
-//create user using: Post {/api/auth}
-router.post('/',[
-   body('name','Enter a valid name').isLength({min:3}),
-   body('email','enter a valid email').isEmail(),
-   body('password','enter a valid password').isLength({min:3}),
+      // .then(user => res.json(user)).catch(err => {console.log(err).res.json({eroor: "please enter unqie value for email"})})
 
-],(req,res)=>{
-   const result = validationResult(req);
-  if (!result.isEmpty()) {
-    return res.status(400).json({ errors: result.array()});
+      res.json(user);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("some Error has ocuured");
+    }
   }
+);
 
-  User.create({
-   name: req.body.name,
-   password: req.body.password,
-   email: req.body.email,
-
-  }).then(user => res.json(user)).catch(err => {console.log(err).res.json({eroor: "please enter unqie value for email"})})
-
-  
-})
-
-module.exports = router
+module.exports = router;
