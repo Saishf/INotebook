537d5964269d679c14a8fddb45bd23646c8e8cfd
Saish Fatangare <fatangaresaish30@gmail.com>
2024-04-05 01:28:14 +0530

    Fetched all notes and added all notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,50 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+var fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/notes");
+const { body, validationResult } = require("express-validator");
 
-router.get('/',(req,res)=>{
-   
-    res.json([])
-})
+//fetch all this notes
+router.get("/fecthnotes", fetchuser, async (req, res) => {
+    try {
+        const userNotes = await Notes.find({ user: req.user.id });
+        res.json(userNotes);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("some Error has ocuured");
+      }
+});
 
-module.exports = router
+//Add new notes using post
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "enter a valid description").isLength({ min: 3 }),
+  ],
+  async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    const note = new Notes({
+      title,
+      description,
+      tag,
+      user: req.user.id,
+    });
+    const savedNote = await note.save();
+
+    res.json(savedNote);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("some Error has ocuured");
+      }
+  }
+);
+
+module.exports = router;
