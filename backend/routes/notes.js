33b31c609222fb546533e560493af69cd27b121b
Saish Fatangare <fatangaresaish30@gmail.com>
2024-04-24 23:36:10 +0530

    Added updates notes feature

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -47,4 +47,29 @@ router.post(
   }
 );
 
+//router 3 update notes 
+router.put( "/updatenotes/:id", fetchuser, async (req, res) =>{
+
+    const { title, description, tag } = req.body;
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    let note = await Notes.findById(req.params.id);
+    if(!note){return res.status(404).send("not found")};
+    if(note.user.toString() !== req.user.id){return res.status(401).send("not allowed")}; 
+
+note = await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
+
+res.json({note});
+}
+
+
+)
+
+
+
+
+
 module.exports = router;
